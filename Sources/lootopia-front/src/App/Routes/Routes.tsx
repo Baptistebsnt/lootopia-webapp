09db436385bb47f6086c81e478763ee01eb4b66f
MalikCmoi <malik.tcheboun@gmail.com>
2025-05-28 14:03:31 +0200

    ajout page profile et modification profile

--- Sources/lootopia-front/src/App/Routes/Routes.tsx
+++ Sources/lootopia-front/src/App/Routes/Routes.tsx
@@ -6,6 +6,7 @@ import ROUTE_URLS from './constants';
 const Home = lazy(() => import('@/pages/Home'));
 const Auth = lazy(() => import('@/pages/Auth'));
 const Maps = lazy(() => import('@/pages/Maps'));
+const Profile = lazy(() => import('@/pages/Profile'));
 
 const RouteCmpt = () => (
   <Suspense fallback={<Loader message="Chargement de la page..." mode={MODES.get} />}>
@@ -13,6 +14,7 @@ const RouteCmpt = () => (
       <Route index path={ROUTE_URLS.HOME} element={<Home />} />
       <Route path={ROUTE_URLS.AUTH} element={<Auth />} />
       <Route path={ROUTE_URLS.MAPS} element={<Maps />} />
+      <Route path={ROUTE_URLS.PROFILE} element={<Profile />} />
     </Routes>
   </Suspense>
 );
--- Sources/lootopia-front/src/pages/Profile/constants.ts
+++ Sources/lootopia-front/src/pages/Profile/constants.ts
@@ -1,2 +1,1 @@
 export const ROUTE_URL_PROFILE = '/profile';
-export const TITLE_BAR_AUTH = 'Profile';
