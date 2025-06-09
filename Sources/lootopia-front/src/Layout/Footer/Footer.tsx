6186b902aba6401bb7812c92b6ccda86d5a828fb
fhennou <fatima.hennou@supdevinci-edu.fr>
2025-06-09 22:15:32 +0200

    style/: Version 1 Page d'accueil

--- Sources/lootopia-front/src/Layout/Footer/Footer.tsx
+++ Sources/lootopia-front/src/Layout/Footer/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = ({
   copyright = `© ${year} Lootopia Tous droits réservés - v${version}`,
 }: TFooter) => {
   return (
-    <footer className={`w-full p-4 bg-gray-50 text-center ${fullScreen ? 'fixed' : 'relative'} bottom-0 left-0`}>
+    <footer className={`w-full p-4  text-center ${fullScreen ? 'fixed' : 'relative'} bottom-0 left-0`}>
       <p>{copyright}</p>
     </footer>
   );
